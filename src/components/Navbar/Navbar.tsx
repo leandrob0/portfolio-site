2fd1d934855fe0bd8cf66265dfae0f35244a8ca8
leandrob0 <leandro.bovino@gmail.com>
2022-07-10 14:58:31 -0300

    project started, navbar prototype done

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+        <section>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+        </section>
+    </nav>
+  )
+}
+
+export default Navbar
