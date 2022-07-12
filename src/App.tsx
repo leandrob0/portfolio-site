3632343e848ff98afb2da525b6bf80b74c6c7d40
leandrob0 <leandro.bovino@gmail.com>
2022-07-12 19:58:44 -0300

    about component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import 'normalize.css';
 
 import Homepage from './components/homepage/Homepage';
+import About from './components/about/About';
 
 function App() {
   return (
     <>
       <Homepage />
+      <About />
     </>
   )
 }
