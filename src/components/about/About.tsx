d1fa0476a95c0afa6065944947c7cacd4d6e4972
leandrob0 <leandro.bovino@gmail.com>
2022-07-12 20:33:28 -0300

    text for the about page added

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,13 +1,20 @@
-import './about.css';
+import "./about.css";
 
 function About() {
   return (
     <div className="about-page">
-        <section className="text-about">
-            
-        </section>
+      <section className="blue-section">
+        <div className="text-about">
+          <h3>Hi! I'm Leandro.</h3>
+          <p>
+            I study in an academic institution and self-taught programming with
+            the goal of becoming a professional developer. My journey started almost 2 years ago, and to this day I am still learning a lot. I am a calm and
+            curious person, perseverance is one of my greatest strengths.
+          </p>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
