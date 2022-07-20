51b0061c34f725b63968ad5ceeedf5828a076620
leandrob0 <leandro.bovino@gmail.com>
2022-07-20 20:15:52 -0300

    projects section added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import 'normalize.css';
 
 import Homepage from './components/homepage/Homepage';
 import About from './components/about/About';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <>
       <Homepage />
       <About />
+      <Projects />
     </>
   )
 }
