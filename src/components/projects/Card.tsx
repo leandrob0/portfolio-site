176a0285a66d8319b92528a2bdd35654174ee981
leandrob0 <leandro.bovino@gmail.com>
2022-07-21 15:48:26 -0300

    text on hover styling done

--- src/components/projects/Card.tsx
+++ src/components/projects/Card.tsx
@@ -1,12 +1,17 @@
 interface cardProps {
   src: string;
+  title: string;
+  description: string;
 }
 
-function Card({ src }: cardProps) {
+function Card({ src, title, description }: cardProps) {
   return (
     <article className="card">
       <img className="image" src={src} alt="Project" />
-      <div className="text-hover"></div>
+      <div className="text-hover">
+        <p>{title}</p>
+        <p className="description">{description}</p>
+      </div>
     </article>
   );
 }
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,22 +1,44 @@
-import Card from './Card';
+import Card from "./Card";
 import Odinbook from "../../images/odinbook-img.png";
 
-import './projects.css';
+import "./projects.css";
 
 function Projects() {
   return (
-    <section className='projects-section'>
-        <h4>My Projects</h4>
-        <p className='projects-subtitle'>Here are some of my most recent/best projects!</p>
-        <div className='projects-container'>
-            <Card src={Odinbook}/>
-            <Card src={Odinbook}/>
-            <Card src={Odinbook}/>
-            <Card src={Odinbook}/>
-            <Card src={Odinbook}/>
-        </div>
+    <section className="projects-section">
+      <h4>My Projects</h4>
+      <p className="projects-subtitle">
+        Here are some of my most recent/best projects!
+      </p>
+      <div className="projects-container">
+        <Card
+          src={Odinbook}
+          title="Odinbook"
+          description="Facebook 'Clon' made as a final project for TheOdinProject.com"
+        />
+        <Card
+          src={Odinbook}
+          title="Odinbook"
+          description="Facebook 'Clon' made as a final project for TheOdinProject.com"
+        />
+        <Card
+          src={Odinbook}
+          title="Odinbook"
+          description="Facebook 'Clon' made as a final project for TheOdinProject.com"
+        />
+        <Card
+          src={Odinbook}
+          title="Odinbook"
+          description="Facebook 'Clon' made as a final project for TheOdinProject.com"
+        />
+        <Card
+          src={Odinbook}
+          title="Odinbook"
+          description="Facebook 'Clon' made as a final project for TheOdinProject.com"
+        />
+      </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
