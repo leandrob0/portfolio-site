97c8f9995d5ac7ba2e090b373aa532e198bccdf6
leandrob0 <leandro.bovino@gmail.com>
2022-07-21 15:40:25 -0300

    animation on project hover done

--- src/components/projects/Card.tsx
+++ src/components/projects/Card.tsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
-
 interface cardProps {
   src: string;
 }
 
 function Card({ src }: cardProps) {
-  const [showText, setShowText] = useState(false);
-
   return (
-    <article
-      className="card"
-      onMouseEnter={() => setShowText(true)}
-      onMouseLeave={() => setShowText(false)}
-    >
+    <article className="card">
       <img className="image" src={src} alt="Project" />
-      {showText && 
-        <div className="text-hover"></div>
-      }
+      <div className="text-hover"></div>
     </article>
   );
 }
